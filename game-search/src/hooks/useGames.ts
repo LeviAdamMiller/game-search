e518c81316754c7060f3fdc800b83762031ed2bc
LeviAdamMiller <LAM614@outlook.com>
2024-08-08 23:12:14 -0700

    Displaying critic score

--- game-search/src/hooks/useGames.ts
+++ game-search/src/hooks/useGames.ts
@@ -14,6 +14,7 @@ export interface Game {
     name: String;
     background_image: string;
     parent_platforms: {platform: Platform}[]
+    metacritic: number;
 }
 
 interface FetchGamesResponse {
